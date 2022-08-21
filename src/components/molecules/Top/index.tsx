ccbeeb295038dc7a4b31b1c6dc2a7d698f3003c0
kota <no.shaking.throne974zero@gmail.com>
2022-08-22 01:57:42 +0900

    トップページのabout部分まで実装

---
+++ src/components/molecules/Top/index.tsx
@@ -0,0 +1,19 @@
+export const Top = () => {
+  return (
+    <div className="fv">
+      <div className="kv-wrapper">
+        <div className="copy-wrapper">
+          <p className="copy_jp">デザインで人を笑顔にする会社</p>
+          <p className="copy_company">DIGSMILE INC.</p>
+          <p className="main-copy">
+            Design
+            <br />
+            for
+            <br />
+            smile.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
